b2d5e82fbbdd7dee87b8ec55dca27bd85fd9c0cd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-07 21:55:26 -0500

    Saving tabprop.R

--- tabprop.R
+++ tabprop.R
@@ -1,12 +1,14 @@
 
-#Count of people in each category for the age
+#count of people in each category for the age
 tab=table(age)
 tab
 
 #percentage of people in each of categories for the age
-prop.table(tab) 
-
-#Count of people in each category for the friendliness attribute.
+pr1=prop.table(tab) 
+pr1
+#count of people in each category for the friendliness attribute.
 tab2=table(friendliness)
 tab2
-prop.table(tab2) 
+
+#percentage of people in each of categories frot the friendliness 
+pr2=prop.table(tab2) 
