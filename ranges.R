e25d528cd2e6ad8d12336e99e636519bf4d83a6c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-07 21:04:23 -0500

    Saving ranges.R

--- ranges.R
+++ ranges.R
@@ -4,5 +4,6 @@ func=function(vec)
     vBuckets <- replicate(length(vec), "Average")
     vBuckets[vec > 7] <- "High"
     vBuckets[vec < 7] <- "Low"
+    return(vBuckets)
 }
 a=func(hotelSurvey$overallCustSat)
