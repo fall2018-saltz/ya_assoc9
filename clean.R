f7a86c09536485758cda376b95c843e445e7e354
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-07 20:44:15 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,6 +2,8 @@
 #load the dataset
 library(jsonlite)
 hotelSurvey=fromJSON("hotelSurveyBarriot.json")
+
+#convert it into a dataframe
 hotelSurvey=data.frame(hotelSurvey)
 str(hotelSurvey)
 hotelSurvey
